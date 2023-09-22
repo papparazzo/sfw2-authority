ec78e947151c77c5661919d0418170561d3c01aa
papparazzo <pappi-@gmx.de>
2023-09-22 06:42:28 +0200

    Authorisation middleware added

---
+++ src/Middleware/Authorisation.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SFW2\Authority\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use SFW2\Authority\Permission\Permission;
+use SFW2\Core\HttpExceptions\HttpForbidden;
+
+class Authorisation implements MiddlewareInterface
+{
+    private Permission $permission;
+
+    public function __construct(Permission $permission)
+    {
+        $this->permission = $permission;
+    }
+
+    /**
+     * @throws HttpForbidden
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        // TODO set "own / all" for crud operations in header
+
+        if(!$this->permission->getActionPermission($pathId, $action)) {
+            throw new HttpForbidden();
+        }
+
+        return $handler->handle($request);
+    }
+}
