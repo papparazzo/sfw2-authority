9f4d07ca50f1fe4f87f0349abd44d45b6cb09260
papparazzo <pappi-@gmx.de>
2025-05-28 04:44:25 +0200

    Refactor Authorization middleware to simplify permission logic

--- src/Middleware/Authorization.php
+++ src/Middleware/Authorization.php
@@ -27,53 +27,30 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-use SFW2\Authority\User;
-use SFW2\Core\HttpExceptions\HttpForbidden;
-use SFW2\Core\HttpExceptions\HttpNotFound;
-use SFW2\Core\Permission\AccessType;
-use SFW2\Core\Permission\PermissionInterface;
-use SFW2\Database\DatabaseException;
-use SFW2\Database\DatabaseInterface;
-use SFW2\Routing\HelperTraits\getRoutingDataTrait;
-use SFW2\Session\SessionInterface;
-use SFW2\Validator\Exception;
+use SFW2\Exception\HttpExceptions\Status4xx\HttpStatus403Forbidden;
+use SFW2\Interoperability\Path\MethodType;
+use SFW2\Interoperability\Path\Path;
+use SFW2\Interoperability\Path\PathMapInterface;
+use SFW2\Interoperability\PermissionInterface;
 
 final class Authorization implements MiddlewareInterface
 {
-    use getRoutingDataTrait;
-
     public function __construct(
         private readonly PermissionInterface $permission,
-        private readonly SessionInterface    $session,
-        private readonly DatabaseInterface   $database
+        private readonly PathMapInterface    $pathMap,
     ) {
     }
 
     /**
-     * @throws HttpForbidden|HttpNotFound
-     * @throws DatabaseException
-     * @throws Exception
+     * @throws HttpStatus403Forbidden
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $pathId = $this->getPathId($request);
-        $action = $this->getAction($request);
+        $pathId = $this->pathMap->getPathId(Path::createFromRequest($request));
 
-        $permission = $this->permission->checkPermission($pathId, $action);
-
-        if ($permission === AccessType::FORBIDDEN) {
-            throw new HttpForbidden();
+        if(!$this->permission->hasPermission($pathId, MethodType::from($request->getMethod()))) {
+            throw new HttpStatus403Forbidden();
         }
-
-        $userId = $this->session->getGlobalEntry(User::class);
-        $user = (new User($this->database))->loadUserById($userId);
-
-        $data['user_id'      ] = $user->getUserId();
-        $data['authenticated'] = $user->isAuthenticated();
-        $data['user_name'    ] = $user->getFullName();
-        $data['restricted'   ] = $permission === AccessType::RESTRICTED;
-
-        $request = $request->withAttribute('sfw2_authority', $data);
 
         return $handler->handle($request);
     }
